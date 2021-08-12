bc9569dbce0f2d9946023923db673a54962408f7
stoodkev <quentin.correa@gmail.com>
2021-08-12 13:46:54 +0800

    update style

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import './App.scss';
 
 const App = () => {
-  return <div className="App">Testt</div>;
+  return (
+    <div className="App">
+      <div id="title">
+        <img src="/assets/img/icon.png" />
+        <span id="text">
+          Splinter<span id="plus">Plus</span>
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default App;
