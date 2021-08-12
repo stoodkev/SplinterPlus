a83095ba5ccd520d945973b08cfc4ac93fe6d39c
stoodkev <quentin.correa@gmail.com>
2021-08-12 14:49:52 +0800

    setup persistor

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,12 +1,15 @@
-import { store } from "@popup/store";
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import App from "./App";
+import { persistor, store } from '@popup/store';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
--- src/popup/store/index.ts
+++ src/popup/store/index.ts
@@ -1,10 +1,20 @@
 import reducers from '@popup/reducers';
 import { applyMiddleware, createStore } from 'redux';
+import { persistReducer } from 'redux-persist';
+import persistStore from 'redux-persist/es/persistStore';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['shouldClaimSps'], // only navigation will be persisted
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
 
-export { store };
+const store = createStore(persistedReducer, applyMiddleware(thunk));
+const persistor = persistStore(store);
+export { store, persistor };
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
